510f65a12712465baf9ab3d9a87b275dcb96e568
jiranimwema202420-lgtm <bepahcontractors@gmail.com>
2026-07-05 21:06:37 +0300

    Remove looping members seed component

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,11 +1,5 @@
 ﻿import MembersClientOnly from "@/components/MembersClientOnly";
-import SeedLocalMembersClientOnly from "@/components/SeedLocalMembersClientOnly";
 
 export default function MembersPage() {
-  return (
-    <div className="space-y-4">
-      <SeedLocalMembersClientOnly />
-      <MembersClientOnly />
-    </div>
-  );
+  return <MembersClientOnly />;
 }
